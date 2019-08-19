bce43dba8472df5d7eceec98f08b60d3c4569d88
Victor Borges <victor.w.developer@gmail.com>
2019-08-19 20:16:45 -0300

    Add Color class

---
+++ src/core/color.cc
@@ -0,0 +1,107 @@
+#include "color.hpp"
+
+namespace Capstone
+{
+
+const Color Color::Black (0, 0, 0);
+const Color Color::White (255, 255, 255);
+const Color Color::Red (255, 0, 0);
+const Color Color::Green (0, 255, 0);
+const Color Color::Blue (0, 0, 255);
+const Color Color::Yellow (255, 255, 0);
+const Color Color::Magenta (255, 0, 255);
+const Color Color::Cyan (0, 255, 255);
+const Color Color::Transparent (0, 0, 0, 0);
+
+// Default constructor
+// This Defines blacks as current color
+Color::Color ():
+  r (0),
+  g (0),
+  b (0),
+  a (255)
+{
+
+}
+
+// This defines each color channel
+Color::Color (int red, int green, int blue, int alpha):
+  r (static_cast<Uint8>(std::min(std::max(red, 0),255))),
+  g (static_cast<Uint8>(std::min(std::max(green, 0),255))),
+  b (static_cast<Uint8>(std::min(std::max(blue, 0),255))),
+  a (static_cast<Uint8>(std::min(std::max(alpha, 0),255)))
+{
+
+}
+
+// This defines the current color as hexadecimal
+Color::Color (Uint32 color):
+  r (static_cast<Uint8>((color & 0xff000000) >> 24)),
+  g (static_cast<Uint8>((color & 0x00ff0000) >> 16)),
+  b (static_cast<Uint8>((color & 0x0000ff00) >> 8)),
+  a (static_cast<Uint8>((color & 0x000000ff) >> 0))
+{
+
+}
+
+// Overload of the binary == operator
+bool operator== (const Color& left, const Color& right)
+{
+  return (left.r == right.r) &&
+         (left.g == right.g) &&
+         (left.b == right.b) &&
+         (left.a == right.a);
+}
+
+// Overload of the binary != operator
+bool operator!= (const Color& left, const Color& right)
+{
+  return !(left == right);
+}
+
+// Overload of the binary + operator
+Color operator+ (const Color& left, const Color& right)
+{
+  return Color(Uint8(std::min(int(left.r) + right.r, 255)),
+               Uint8(std::min(int(left.g) + right.g, 255)),
+               Uint8(std::min(int(left.b) + right.b, 255)),
+               Uint8(std::min(int(left.a) + right.a, 255)));
+}
+
+// Overloads of the binary - operator
+Color operator- (const Color& left, const Color& right)
+{
+  return Color(Uint8(std::max(int(left.r) - right.r, 0)),
+               Uint8(std::max(int(left.g) - right.g, 0)),
+               Uint8(std::max(int(left.b) - right.b, 0)),
+               Uint8(std::max(int(left.a) - right.a, 0)));
+}
+
+// Overloads of the binary * operator
+Color operator* (const Color& left, const Color& right)
+{
+  return Color(Uint8(int(left.r) * right.r / 255),
+               Uint8(int(left.g) * right.g / 255),
+               Uint8(int(left.b) * right.b / 255),
+               Uint8(int(left.a) * right.a / 255));
+}
+
+// Overloads of the binary += operator
+Color& operator+= (Color& left, const Color& right)
+{
+  return left = left + right;
+}
+
+// Overloads of the binary -= operator
+Color& operator-= (Color& left, const Color& right)
+{
+  return left = left - right;
+}
+
+// Overloads of the binary *= operator
+Color& operator*= (Color& left, const Color& right)
+{
+  return left = left * right;
+}
+
+} // namespace Capstone
---
+++ src/core/color.hpp
@@ -0,0 +1,217 @@
+#ifndef CPP_SNAKE_GAME_CORE_COLOR_HPP
+#define CPP_SNAKE_GAME_CORE_COLOR_HPP
+
+#include <SDL2/SDL_stdinc.h>
+#include <SDL2/SDL_pixels.h>
+
+namespace Capstone
+{
+/**
+ * This defines a color that can be used by SDL functions
+ *
+ * This class was strongly inspired by SFML's Color. If you'd like to check
+ * the original code, please take a look at:
+ *
+ * https://github.com/SFML/SFML/blob/master/include/SFML/Graphics/Color.hpp
+ * https://github.com/SFML/SFML/blob/master/src/SFML/Graphics/Color.cpp
+ */
+class Color
+{
+ public:
+
+  /**
+   * Default constructor
+   *
+   * This defines the current color as black
+   */
+  Color();
+
+  /**
+   * This defines the current color
+   *
+   * It allows the current class to define each channel with values between
+   * 0 and 255
+   *
+   * @param red    Red color channel
+   * @param green  Green color channel
+   * @param blue   Blue color channel
+   * @param alpha  Alpha color channel
+   */
+  Color(int red, int green, int blue, int alpha = 255);
+
+  /**
+   * It allows the current class to define the color as hexadecimal
+   *
+   * Instead of setting each channel color, this constructor uses a single
+   * value as argument, defining the four channels as hexadecimal as follows:
+   *
+   *  auto dodge_blue = Color(0x0084ffff);
+   *
+   * @param color  The color as hexadecimal value
+   */
+  explicit Color(Uint32 color);
+
+  /**
+   * The most common colors
+   *
+   * They are defined as static variables making possible to use Color::White,
+   * as a shortcut to Color(255, 255, 255), for example.
+   */
+  static const Color Black;
+  static const Color White;
+  static const Color Red;
+  static const Color Green;
+  static const Color Blue;
+  static const Color Yellow;
+  static const Color Magenta;
+  static const Color Cyan;
+  static const Color Transparent;
+
+ public:
+
+  /**
+   * This defines all color channels as public
+   *
+   * All values can be assigned with values between 0 and 255
+   */
+  Uint8  r; // red
+  Uint8  g; // green
+  Uint8  b; // blue
+  Uint8  a; // alpha (opacity)
+};
+
+/**
+ * Overload of the == operator
+ *
+ * This operator compares two colors and check if they are equal.
+ *
+ * @param left   Left operand
+ * @param right  Right operand
+ * @returns boolean  Returns true if colors are equal, false if they are different
+ */
+bool operator ==(const Color &left, const Color &right);
+
+/**
+ * Overload of the != operator
+ *
+ * This operator compares two colors and check if they are different.
+ *
+ * Returns true if colors are different, false if they are equal
+ *
+ * @param left   Left operand
+ * @param right  Right operand
+ * @returns boolean
+ */
+bool operator !=(const Color &left, const Color &right);
+
+/**
+ * Overload of the binary + operator
+ *
+ * This operator returns the component-wise sum of two colors.
+ * Components that exceed 255 are clamped to 255.
+ *
+ * it allows lighten colors like:
+ *
+ *   auto new_color = Color::Red + Color(50, 50, 50) // Color(255, 50, 50, 255);
+ *
+ * @param left  Left operand
+ * @param right Right operand
+ *
+ * @returns Color
+ */
+Color operator +(const Color &left, const Color &right);
+
+/**
+ * Overload of the binary - operator
+ *
+ * This operator returns the component-wise subtraction of two colors.
+ * Components that exceed 255 are clamped to 255.
+ *
+ * It allows darken colors like:
+ *
+ *   auto new_color = Color::Red - Color(50, 50, 50, 0) // Color(205, 0, 0, 255);
+ *
+ * @param left  Left operand
+ * @param right Right operand
+ *
+ * @returns Color
+ */
+Color operator -(const Color &left, const Color &right);
+
+/**
+ * Overload of the binary * operator
+ *
+ * This operator returns the component-wise multiplication of two colors.
+ * Components that exceed 255 are clamped to 255.
+ *
+ * It creates darken colors like:
+ *
+ *   auto new_color = Color::Red * Color(50, 50, 50) // Color(50, 0, 0, 255);
+ *
+ * @param left  Left operand
+ * @param right Right operand
+ *
+ * @returns Color
+ */
+Color operator *(const Color &left, const Color &right);
+
+/**
+ * Overload of the binary += operator
+ *
+ * This operator computes the component-wise sum of two colors,
+ * and assigns the result to the left operand.
+ * Components that exceed 255 are clamped to 255.
+ *
+ * It creates lighten colors like:
+ *
+ *   auto current_color = Color::Red;
+ *   current_color += Color(50, 50, 50) // Color(255, 50, 50, 255);
+ *
+ * @param left  Left operand
+ * @param right Right operand
+ *
+ * @returns Color
+ */
+Color& operator +=(Color &left, const Color &right);
+
+/**
+ * Overload of the binary -= operator
+ *
+ * This operator computes the component-wise subtract of two colors,
+ * and assigns the result to the left operand.
+ * Components that exceed 255 are clamped to 255.
+ *
+ * It creates darken colors like:
+ *
+ *   auto current_color = Color::Red;
+ *   current_color -= Color(50, 50, 50) // Color(205, 0, 0, 255);
+ *
+ * @param left  Left operand
+ * @param right Right operand
+ *
+ * @returns Color
+ */
+Color& operator -=(Color &left, const Color &right);
+
+/**
+ * Overload of the binary *= operator
+ *
+ * This operator computes the component-wise multiplication of two colors,
+ * and assigns the result to the left operand.
+ * Components that exceed 255 are clamped to 255.
+ *
+ * It creates darken colors like:
+ *
+ *   auto current_color = Color::Red;
+ *   current_color *= Color(50, 50, 50) // Color(50, 0, 0, 255);
+ *
+ * @param left  Left operand
+ * @param right Right operand
+ *
+ * @returns Color
+ */
+Color& operator *=(Color &left, const Color &right);
+
+} // namespace Capstone
+
+#endif // CPP_SNAKE_GAME_CORE_COLOR_HPP
