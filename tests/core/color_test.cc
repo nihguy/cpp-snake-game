f7313c1382919f2478f0b61da76eea76edaa6988
Victor Borges <victor.w.developer@gmail.com>
2019-08-24 13:39:33 -0300

    Improve the test description

--- tests/core/color_test.cc
+++ tests/core/color_test.cc
@@ -1,233 +1,161 @@
 #include "../../src/core/color.hpp"
+
 #include "gtest/gtest.h"
 
 namespace
 {
 
-// This tests if constructor can assign the value correctly
-TEST(Color, should_assign_color_channel_on_constructor) {
-  Capstone::Color white {255, 255, 255};
+using ::Capstone::Color;
 
-  ASSERT_EQ (255, white.r); // Red channel
-  ASSERT_EQ (255, white.g); // Green channel
-  ASSERT_EQ (255, white.b); // Blue channel
+TEST(Color, DefaultConstructor_ShouldTheColorBeBlack)
+{
+  Color color{};
 
-  // The alpha channel is assigned as 255 as default
-  EXPECT_EQ (255, white.a);
+  ASSERT_EQ (color.r, 0) << "The red channel should be zero because default constructor is called";
+  ASSERT_EQ (color.g, 0) << "The green channel should be zero because default constructor is called";
+  ASSERT_EQ (color.b, 0) << "The blue channel should be zero because default constructor is called";
+  ASSERT_EQ (color.a, 255) << "The alpha channel should be 255 because default constructor is called";
 }
 
-TEST(Color, should_assigning_alpha_channel_as_optional_argument) {
-  // The fourth argument should assign alpha channel
-  Capstone::Color transparent {255, 255, 255, 0};
+TEST(Color, ParameterContructor_ShouldAssignEachColorChannel) {
+  Color color{100, 150, 200};
 
-  EXPECT_EQ (255, transparent.r); // Red channel
-  EXPECT_EQ (255, transparent.g); // Green channel
-  EXPECT_EQ (255, transparent.b); // Blue channel
+  ASSERT_EQ (color.r, 100) << "The alpha channel should be 100 because it's assigned to parameter constructor is called";
+  ASSERT_EQ (color.g, 150) << "The green channel should be 150 because it's assigned to parameter constructor is called";
+  ASSERT_EQ (color.b, 200) << "The blue channel should be 200 because it's assigned to parameter constructor is called";
 
-  ASSERT_EQ (0, transparent.a); // Alpha channel
+  ASSERT_EQ (color.a, 255) << "The alpha channel should be 255 because it's not defined in parameter constructor";
 }
 
-TEST(Color, should_clamp_the_channels_to_255_when_it_exceeds_on_constructor) {
-  Capstone::Color white (300, 300, 300, 300);
+TEST(Color, ParameterContructor_ShouldAssignTheAlphaChannelAsOptionalArgument) {
+  Color color{100, 150, 200, 0};
 
-  ASSERT_EQ (255, white.r); // Red channel
-  ASSERT_EQ (255, white.g); // Green channel
-  ASSERT_EQ (255, white.b); // Blue channel
-  ASSERT_EQ (255, white.a); // Alpha channel
+  ASSERT_EQ (color.r, 100) << "The red channel should be 100 because it's assigned to parameter constructor is called";
+  ASSERT_EQ (color.g, 150) << "The green channel should be 150 because it's assigned to parameter constructor is called";
+  ASSERT_EQ (color.b, 200) << "The blue channel should be 200 because it's assigned to parameter constructor is called";
+
+  ASSERT_EQ (color.a, 0) << "The alpha channel should be 0 because it's assigned to parameter constructor is called";
 }
 
-TEST(Color, should_clamp_the_channels_to_0_when_it_lower_on_constructor) {
-  Capstone::Color black (-255, -255, -255, -255);
+TEST(Color, Channels_ShouldClampEachOneTo255WhenItExceeds) {
+  Color color{300, 400, 500, 600};
 
-  ASSERT_EQ (0, black.r); // Red channel
-  ASSERT_EQ (0, black.g); // Green channel
-  ASSERT_EQ (0, black.b); // Blue channel
-  ASSERT_EQ (0, black.a); // Alpha channel
+  ASSERT_EQ (color.r, 255) << "The red channel should be clamped to 255 because the parameter constructor argument exceeds";
+  ASSERT_EQ (color.g, 255) << "The green channel should be clamped to 255 because the parameter constructor argument exceeds";
+  ASSERT_EQ (color.b, 255) << "The blue channel should be clamped to 255 because the parameter constructor argument exceeds";
+  ASSERT_EQ (color.a, 255) << "The alpha channel should be clamped to 255 because the parameter constructor argument exceeds";
 }
 
-TEST(Color, should_assign_color_using_hexadecimal_on_constructor) {
-  // The two last numbers represents the alpha channel
-  Capstone::Color red (0xFF0000FF);
+TEST(Color, Channels_ShouldClampEachChannelsToZeroWhenItIsInferior) {
+  Color color{-100, -200, -300, -400};
 
-  ASSERT_EQ (255, red.r); // Red channel
-  ASSERT_EQ (0, red.g); // Green channel
-  ASSERT_EQ (0, red.b); // Blue channel
-  ASSERT_EQ (255, red.a); // Alpha channel
+  ASSERT_EQ (color.r, 0) << "The red channel should be clamped to zero because the parameter constructor argument is lower of it";
+  ASSERT_EQ (color.g, 0) << "The green channel should be clamped to zero because the parameter constructor argument is lower of it";
+  ASSERT_EQ (color.b, 0) << "The blue channel should be clamped to zero because the parameter constructor argument is lower of it";
+  ASSERT_EQ (color.a, 0) << "The alpha channel should be clamped to zero because the parameter constructor argument is lower of it";
 }
 
-TEST(Color, should_constructor_to_define_black_as_default_color) {
-  // This defines "solid" black (with alpha channel as 255)
-  Capstone::Color black;
+TEST(Color, SingleParameterContructor_ShouldAssignColorAsHexadecimal) {
+  Color color{0xCCDDEEFF};
 
-  ASSERT_EQ (0, black.r); // Red channel
-  ASSERT_EQ (0, black.g); // Green channel
-  ASSERT_EQ (0, black.b); // Blue channel
-  ASSERT_EQ (255, black.a); // Alpha channel
+  ASSERT_EQ (color.r, 204) << "The red channel should be 204 because the first hexadecimal byte is defined as `CC` on parameter constructor";
+  ASSERT_EQ (color.g, 221) << "The green channel should be 221 because the second hexadecimal byte is defined as `DD` on parameter constructor";
+  ASSERT_EQ (color.b, 238) << "The blue channel should be 204 because the third hexadecimal byte is defined as `EE` on parameter constructor";
+  ASSERT_EQ (color.a, 255) << "The alpha channel should be 255 because the fourth hexadecimal byte is defined as `FF` on parameter constructor";
 }
 
-TEST(Color, should_color_be_compared_with_another_one) {
-  // For the sake of readability
-  using namespace Capstone;
+TEST(Color, IsEqualToAndIsNotEqualToOperators_ShouldAColorComparedToEachOther) {
+  Color color{255, 255, 255, 255};
 
-  // This is the main color
-  Color black1;
-
-  // There are colors will be compared to the above one
-  Color black2;
-  Color red {255, 0, 0};
-
-  // Should be true
-  ASSERT_TRUE (black1 == black2); // When asserting same colors
-  ASSERT_TRUE (black1 != red); // When checking if different colors are equals
-
-  // Should be false
-  ASSERT_FALSE (black1 == red); // When comparing different colors
-  ASSERT_FALSE (black1 != black2); // When checking if the same colors are equals
+  ASSERT_TRUE (color == Color(255, 255, 255, 255)) << "Should be true to `==` because each channel is equivalent";
+  ASSERT_TRUE (color != Color(255, 255, 255, 0))  << "Should be true to `!=` because one of the channels is not equivalent";
 }
 
-TEST(Color, should_color_offers_the_most_common_color_as_static_variables) {
-  // For the sake of readability
-  using namespace Capstone;
-
-  // This defines the "full" White color
-  ASSERT_TRUE (Color::White == Color (255, 255, 255, 255));
-
-  // This defines the "RGB" primary colors
-  ASSERT_TRUE (Color::Red == Color (255, 0, 0, 255));
-  ASSERT_TRUE (Color::Green == Color (0, 255, 0, 255));
-  ASSERT_TRUE (Color::Blue == Color (0, 0, 255, 255));
-
-  // This defines the "CMYK" primary colors
-  ASSERT_TRUE (Color::Cyan ==  Color (0, 255, 255, 255));
-  ASSERT_TRUE (Color::Magenta == Color (255, 0, 255, 255));
-  ASSERT_TRUE (Color::Yellow == Color (255, 255, 0, 255));
-  ASSERT_TRUE (Color::Black == Color (0, 0, 0, 255));
-
-  // Transparent (total opacity)
-  ASSERT_TRUE(Color::Transparent == Color(0, 0, 0, 0));
+TEST(Color, ShouldOffersTheMostCommonColorAsStaticVariables)
+{
+  ASSERT_TRUE (Color::White == Color(255, 255, 255, 255)) << "Should be true because they both have the same channels";
+  ASSERT_TRUE (Color::Red == Color (255, 0, 0, 255)) << "Should be true because they both have the same channels";
+  ASSERT_TRUE (Color::Green == Color (0, 255, 0, 255)) << "Should be true because they both have the same channels";
+  ASSERT_TRUE (Color::Blue == Color (0, 0, 255, 255)) << "Should be true because they both have the same channels";
+  ASSERT_TRUE (Color::Cyan == Color (0, 255, 255, 255)) << "Should be true because they both have the same channels";
+  ASSERT_TRUE (Color::Magenta == Color (255, 0, 255, 255)) << "Should be true because they both have the same channels";
+  ASSERT_TRUE (Color::Yellow == Color (255, 255, 0, 255)) << "Should be true because they both have the same channels";
+  ASSERT_TRUE (Color::Black == Color (0, 0, 0, 255)) << "Should be true because they both have the same channels";
+  ASSERT_TRUE (Color::Transparent == Color(0, 0, 0, 0)) << "Should be true because they both have the same channels";
 }
 
-TEST(Color, should_create_a_new_darken_color_when_subtract_two_of_them) {
-  // For the sake of readability
-  using namespace Capstone;
+TEST(Color, SubtractionOperator_ShouldCreateANewDarkenColor) {
+  Color transparent = (Color::White - Color::White);
+  ASSERT_TRUE (Color(0, 0, 0, 0) == transparent) << "Should remove all the opacity channel by default";
 
-  // Should remove 255 from Yellow's green and alpha channel
-  Color new_red_color = Color::Yellow - Color::Green;
-  ASSERT_EQ(new_red_color, Color(255, 0, 0, 0)); // Yellow - Green = Transparent Red color
+  Color solid = (Color::Red - Color(255, 0, 0, 0));
+  ASSERT_TRUE (Color(0, 0, 0, 255) == solid) << "Should not remove the opacity channel because one of them is zero";
 
-  // Should subtract 127 from each color channel and
-  // preserve the opacity channel because it's zero
-  Color new_gray_color = Color::White - Color(127, 127, 127, 0);
-  ASSERT_EQ(new_gray_color, Color(128, 128, 128, 255));
+  Color darken = (Color::White - Color(127, 127, 127, 0));
+  ASSERT_TRUE (Color(128, 128, 128, 255) == darken) << "Should darken color from white to grey";
 
-  // Should subtract when a color is defined as hexadecimal
-  Color new_gray_color_from_hex = Color::White - Color(0x7F7F7F00);
-  ASSERT_EQ(new_gray_color_from_hex, Color(128, 128, 128, 255));
-
-  // Should clamp the value to 0 when it's lower than zero
-  Color black = Color(0, 0, 0, 255) - Color(255, 255, 0, 0);
-  ASSERT_EQ(black, Color(0, 0, 0, 255));
+  Color clamped = (Color(0, 0, 0, 255) - Color(255, 255, 0, 255));
+  ASSERT_TRUE (Color(0, 0, 0, 0) == clamped) << "Should clamp each channel lower than zero to zero";
 }
 
-TEST(Color, should_create_a_new_lighten_color_when_add_two_of_them) {
-  // For the sake of readability
-  using namespace Capstone;
-
-  // Should add 255 from each color channel.
-  // The values exceeds 255 will be clamped to this value
+TEST(Color, AdditionOperator_ShouldCreateANewLightenColor) {
   Color clamped = Color::White + Color::White;
-  ASSERT_EQ(clamped, Color::White);
+  ASSERT_EQ(clamped, Color::White) << "Should clamp each channel higher than 255 to 255";
 
-  // Should sum 255 to Yellow's blue channel
-  // The values exceeds 255 will be clamped to this value
   Color white = Color::Yellow + Color::Blue;
-  ASSERT_EQ(white, Color::White);
+  ASSERT_EQ(white, Color::White) << "Should add 255 to blue channel creating a new white color";
 
-  // Should add 128 from each color channel and keep the alpha channel
-  Color gray = Color::Black + Color(128, 128, 128, 0);
-  ASSERT_EQ(gray, Color(128, 128, 128, 255));
-
-  // Should lighten color when a color is defined as hexadecimal as well
-  Color gray_from_hex = Color::Black + Color(0x80808000);
-  ASSERT_EQ(gray_from_hex, Color(128, 128, 128, 255));
+  Color gray = Color::Black + Color(128, 128, 128);
+  ASSERT_EQ(gray, Color(128, 128, 128, 255)) << "Should create lighten grey from black color adding 128 to each channel";
 }
 
-TEST(Color, should_create_a_new_darken_color_when_multiply_two_of_them) {
-  // For the sake of readability
-  using namespace Capstone;
-
-  // Should multiply each color channel.
-  // The values exceeds 255 will be clamped to this value
+TEST(Color, MultiplicationOperator_ShouldCreateANewDarkenColor) {
   Color dark_red = Color::Red * Color(50, 50, 50);
-  ASSERT_EQ(dark_red, Color(50, 0, 0, 255));
+  ASSERT_EQ(dark_red, Color(50, 0, 0, 255)) << "Should create a darken red color multiplying each channel to 50";
 
-  // Should sum 255 to Yellow's blue channel
-  // The values exceeds 255 will be clamped to this value
   Color dark_gray = Color(80, 80, 80) * Color(33, 33, 33);
-  ASSERT_EQ(dark_gray, Color(10, 10, 10, 255));
-
-  // Should darken each Color's channel a color is defined as hexadecimal as well
-  Color dark_gray_from_hex = Color(80, 80, 80) * Color(0x212121FF);
-  ASSERT_EQ(dark_gray_from_hex, Color(10, 10, 10, 255));
+  ASSERT_EQ(dark_gray, Color(10, 10, 10, 255)) << "Should create a darken gray color multiplying each channel to 33";
 }
 
-TEST(Color, should_lighten_the_current_color_when_addition_assignment_is_used) {
-  // For the sake of readability
-  using namespace Capstone;
+TEST(Color, AdditionAssignmentOperator_ShouldLightenTheCurrentColor)
+{
+  auto clamped = Color::White;
+  clamped += Color::White;
+  ASSERT_EQ(clamped, Color::White) << "Should clamp each channel higher than 255 to 255";
 
-  // Should lighten the current color adding the right value.
-  Color black = Color::Black;
-  black += Color(128, 128, 128);
-  ASSERT_EQ(black, Color(128, 128, 128, 255));
+  auto yellow = Color::Yellow;
+  yellow += Color::Blue;
+  ASSERT_EQ(yellow, Color::White) << "Should add 255 to the current blue channel lighten the color from yellow to white";
 
-  // Should lighten the current color adding the right value.
-  // The values exceeds 255 will be clamped to this value
-  Color red = Color::Red;
-  red += Color(50, 50, 50);
-  ASSERT_EQ(red, Color(255, 50, 50, 255));
-
-  // Should lighten the current color adding an hexadecimal as well
-  Color green = Color::Green;
-  green += Color(0xFF0000FF);
-  ASSERT_EQ(green, Color::Yellow);
+  auto black = Color::Black;
+  black +=  Color(128, 128, 128);
+  ASSERT_EQ(black, Color(128, 128, 128, 255)) << "Should lighten the black to grey color adding 128 to each channel";
 }
 
-TEST(Color, should_darken_the_current_color_when_subtraction_assignment_is_used) {
-  // For the sake of readability
-  using namespace Capstone;
+TEST(Color, SubtractionAssignmentOperator_ShouldDarkenTheCurrentColor)
+{
+  auto white = Color::White;
+  white -= Color(55, 55, 55);
+  ASSERT_EQ(white, Color(200, 200, 200, 0)) << "Should remove all the opacity channel by default";
 
-  // Should darken the current color (including alpha) when
-  // subtracting the right value
-  Color white = Color::White;
-  white -= Color(55, 55, 55, 255);
-  ASSERT_EQ(white, Color(200, 200, 200, 0));
+  auto solid = Color::White;
+  solid -= Color(55, 55, 55, 0);
+  ASSERT_TRUE (Color(200, 200, 200, 255) == solid) << "Should not remove the opacity channel because one of them is zero";
 
-  // Should clamp the value of each color channel to zero if it exceeds this value
-  // Should keep the alpha channel because te right value of this is zero
-  Color clamped = Color::Black;
-  clamped -= Color(128, 128, 128, 0);
-  ASSERT_EQ(clamped, Color(0, 0, 0, 255));
+  auto darken = Color::White;
+  darken -= Color(127, 127, 127, 0);
+  ASSERT_TRUE (Color(128, 128, 128, 255) == darken) << "Should darken the current color from white to grey";
 
-  // Should darken the current color subtracting an hexadecimal as well
-  Color red = Color::Red;
-  red -= Color(0xFF000000);
-  ASSERT_EQ(red, Color::Black);
+  auto clamped = Color::Black;
+  clamped -= Color(255, 255, 255, 0);
+  ASSERT_EQ(clamped, Color(0, 0, 0, 255)) << "Should clamp each channel lower than zero to zero";
 }
 
-TEST(Color, should_darken_the_current_color_when_multiplication_assignment_is_used) {
-  // For the sake of readability
-  using namespace Capstone;
-
-  // Should darken the current color (including alpha) when
-  // subtracting the right value clamp the values to 255 if it exceeds.
+TEST(Color, MultiplicationAssignmentOperator_ShouldDarkenTheCurrentColor)
+{
   Color white = Color::White;
   white *= Color(128, 128, 128, 255);
-  ASSERT_EQ(white, Color(128, 128, 128, 255));
-
-  // Should darken the current color multiplying an hexadecimal as well
-  Color red = Color::Red;
-  red *= Color(0x800000FF);
-  ASSERT_EQ(red, Color(128, 0, 0, 255));
+  ASSERT_EQ(white, Color(128, 128, 128, 255)) << "Should darken the current color (including alpha) and clamp the values to 255";
 }
 
-}
+} // namespace
