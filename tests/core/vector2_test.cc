3298cf705bba270753cd64f5c32831c5f8165b65
Victor Borges <victor.w.developer@gmail.com>
2019-08-24 05:16:10 -0300

    Improve the Vector2 test

--- tests/core/vector2_test.cc
+++ tests/core/vector2_test.cc
@@ -1,189 +1,157 @@
 #include "../../src/core/vector2.hpp"
+
 #include "gtest/gtest.h"
 
 namespace
 {
 
-TEST(Vector2, should_define_coordinates_as_zero_by_default) {
-  // For the sake of readability
-  using namespace Capstone;
+/**
+ * This allows the Vector2 to avoid repeat each test for each template type.
+ *
+ * @tparam T  The type that will be assigned automatically by the GTest
+ */
+template <typename T>
+class Vector2Test: public ::testing::Test
+{
+ protected:
 
-  // The int vector should defines zero to coordinates by default
-  Vector2<int> ivector2;
-  ASSERT_EQ (0, ivector2.x); // X coordinate
-  ASSERT_EQ (0, ivector2.y); // Y coordinate
+  /**
+   * It allows this class to assign the Vector2's pointer to the variable `vector2` in other to automatic
+   * convert the template class using GTest template features.
+   *
+   * @param vector2  A pointer to the Dynamic Vector2 type
+   */
+  void Assign (::Capstone::Vector2<T> *vector2) {
+    this->vector2 = ::std::shared_ptr<::Capstone::Vector2<T>> (vector2);
+  }
+
+ protected:
+  /**
+   * This holds the Vector2 instance used by the test properly
+   */
+  std::shared_ptr<Capstone::Vector2<T>> vector2;
+};
+
+/**
+ * This defines the most common types used in Vector2. It allows the GTest
+ * to convert the class automatically, so we can define the test just once
+ * and check the results for each Vector2's type.
+ */
+using Vector2Types = ::testing::Types<int, unsigned int, float>;
+TYPED_TEST_SUITE (Vector2Test, Vector2Types);
 
 
-  // The unsigned int vector should defines zero to coordinates by default
-  Vector2<unsigned int> uvector2;
-  ASSERT_EQ (0, uvector2.x); // X coordinate
-  ASSERT_EQ (0, uvector2.y); // Y coordinate
+TYPED_TEST(Vector2Test, DefaultContructor_ShouldDefineTheCoordinatesAsZero)
+{
+  this->Assign (new ::Capstone::Vector2<TypeParam>{});
 
-  // The float vector should defines zero to coordinates by default
-  Vector2<float> fvector2;
-  ASSERT_EQ (0, fvector2.x); // X coordinate
-  ASSERT_EQ (0, fvector2.y); // Y coordinate
+  ASSERT_EQ(this->vector2->x, 0);
+  ASSERT_EQ(this->vector2->y, 0);
 }
 
-TEST(Vector2, should_assign_the_same_value_to_coordinates_when_constructor_has_one_argument) {
-  // For the sake of readability
-  using namespace Capstone;
+TYPED_TEST(Vector2Test, ParameterContructor_ShouldCastArgumentValueUsingTemplateArgumentDeductionType)
+{
+  this->Assign (new ::Capstone::Vector2<TypeParam> (-0.5f, -0.5f));
 
-  // Should defines the same value
-  Vector2<int> ivector2{1000};
-  ASSERT_EQ (1000, ivector2.x); // X coordinate
-  ASSERT_EQ (1000, ivector2.y); // Y coordinate
+  ASSERT_EQ(this->vector2->x, static_cast<TypeParam>(-0.5f)) << "Vector2 should cast the `x` argument value to the deduction type";
+  ASSERT_EQ(this->vector2->y, static_cast<TypeParam>(-0.5f)) << "Vector2 should cast the `y` argument value to the deduction type";
 
-  // Should defines the same value
-  Vector2<unsigned int> uvector2{1000};
-  ASSERT_EQ (1000, uvector2.x); // X coordinate
-  ASSERT_EQ (1000, uvector2.y); // Y coordinate
-
-  // Should defines the same value
-  Vector2<float> fvector2{1000.5f};
-  ASSERT_EQ (1000.5f, fvector2.x); // X coordinate
-  ASSERT_EQ (1000.5f, fvector2.y); // Y coordinate
+  if (typeid (TypeParam) == typeid (float))
+  {
+    ASSERT_FLOAT_EQ (this->vector2->x, -0.5f) << "Vector2<float> should keep the negative float value to `x` coordinate";
+    ASSERT_FLOAT_EQ (this->vector2->y, -0.5f) << "Vector2<float> should keep the negative float value to `y` coordinate";
+  }
+  else
+  {
+    ASSERT_NE (this->vector2->x, -0.5f) << "Vector2<int> and Vector2<unsigned int> should cast -0.5f to zero on `x` coordinate";
+    ASSERT_NE (this->vector2->y, -0.5f) << "Vector2<int> and Vector2<unsigned int> should cast -0.5f to zero on `y` coordinate";
+  }
 }
 
-TEST(Vector2, should_convert_a_vector_type_to_another_using_constructor) {
-  // For the sake of readability
-  using namespace Capstone;
+TYPED_TEST(Vector2Test, SingleParameterContructor_ShouldAssignTheSameValueToTheCoordinates)
+{
+  this->Assign (new ::Capstone::Vector2<TypeParam> (10));
 
-  Vector2<int> ivector2{Vector2<float>{1.5f}};
+  ASSERT_TRUE(this->vector2->x == this->vector2->y) << "The coordinates should be equal when the single parameter constructor is called";
 
-  // Should converts float to int
-  ASSERT_EQ (1, ivector2.x); // X coordinate
-  ASSERT_EQ (1, ivector2.y); // Y coordinate
+  ASSERT_EQ(this->vector2->x, 10) << "The `x` coordinate value should be the same assigned as single parameter constructor argument";
+  ASSERT_EQ(this->vector2->y, 10) << "The `y` coordinate value should be the same assigned as single parameter constructor argument";
 }
 
-TEST(Vector2, should_define_the_two_coordinates_on_constructor) {
-  // For the sake of readability
-  using namespace Capstone;
+TYPED_TEST(Vector2Test, CopyContructor_ShouldCastTheCopiedVector2ToTheCurrentType)
+{
+  this->Assign (new ::Capstone::Vector2<TypeParam> (::Capstone::Vector2<float>(-0.5f)));
 
-  // The first argument represents the X coordinate
-  // and the second one represents the Y coordinates
-  Vector2<int> ivector2{10, 20};
-  ASSERT_EQ (10, ivector2.x); // X coordinate
-  ASSERT_EQ (20, ivector2.y); // Y coordinate
-
-  Vector2<unsigned int> uvector2{10, 20};
-  ASSERT_EQ (10, uvector2.x); // X coordinate
-  ASSERT_EQ (20, uvector2.y); // Y coordinate
-
-  Vector2<float> fvector2{10.5f, 20.5f};
-  ASSERT_EQ (10.5f, fvector2.x); // X coordinate
-  ASSERT_EQ (20.5f, fvector2.y); // Y coordinate
+  if (typeid (TypeParam) == typeid (float))
+  {
+    ASSERT_FLOAT_EQ (this->vector2->x, -0.5f) << "Vector2<float> should keep the negative float value to `x` variable";
+    ASSERT_FLOAT_EQ (this->vector2->y, -0.5f) << "Vector2<float> should keep the negative float value to `y` variable";
+  }
+  else
+  {
+    ASSERT_EQ (this->vector2->x, 0) << "Vector2<int> and Vector2<unsigned int> should cast -0.5f to zero on `x` coordinate";
+    ASSERT_EQ (this->vector2->y, 0) << "Vector2<int> and Vector2<unsigned int> should cast -0.5f to zero on `y` coordinate";
+  }
 }
 
-TEST(Vector2, should_reassign_each_coordinate_using_public_variables) {
-  // For the sake of readability
-  using namespace Capstone;
+TYPED_TEST(Vector2Test, Cordinates_ShouldBeReassigned)
+{
+  this->Assign (new ::Capstone::Vector2<TypeParam> {});
 
-  // This defines vector of int type
-  Vector2<int> ivector2;
-  ivector2.x = 10;
-  ivector2.y = 20;
+  ASSERT_EQ(this->vector2->x, 0) << "The Vector2's x coordinate should be zero because default constructor is called";
+  ASSERT_EQ(this->vector2->y, 0) << "The Vector2's y coordinate should be zero because default constructor is called";
 
-  ASSERT_EQ (10, ivector2.x); // X coordinate
-  ASSERT_EQ (20, ivector2.y); // Y coordinate
+  this->vector2->x = 1;
+  this->vector2->y = 1;
 
-
-  // This defines vector of unsigned int type
-  Vector2<unsigned int> uvector2;
-  uvector2.x = 10;
-  uvector2.y = 20;
-
-  ASSERT_EQ (10, uvector2.x); // X coordinate
-  ASSERT_EQ (20, uvector2.y); // Y coordinate
-
-
-  // This defines vector of float type
-  Vector2<float> fvector2;
-  fvector2.x = 10.5f;
-  fvector2.y = 20.5f;
-
-  ASSERT_EQ (10.5f, fvector2.x); // X coordinate
-  ASSERT_EQ (20.5f, fvector2.y); // Y coordinate
+  ASSERT_EQ(this->vector2->x, 1) << "The Vector2's x coordinate should be reassigned to 1";
+  ASSERT_EQ(this->vector2->y, 1) << "The Vector2's y coordinate should be reassigned to 1";
 }
 
-TEST(Vector2, should_vector2_be_compared_with_another_one_with_the_same_type) {
-  // For the sake of readability
-  using namespace Capstone;
+TYPED_TEST(Vector2Test, Vector2_ShouldBeComparedToEachOther)
+{
+  this->Assign (new ::Capstone::Vector2<TypeParam> {1, 2});
 
-  // Should be true because they both have the same values and type
-  ASSERT_TRUE (Vector2<int>(100, 200) == Vector2<int>(100, 200));
+  auto sameCoordinates = ::Capstone::Vector2<double>(1.0, 2.0);
+  auto diffCoordinates = ::Capstone::Vector2<double>(1.0, 20.0);
 
-  // Should be false because they both have the same values
-  ASSERT_FALSE (Vector2<int>(100, 200) != Vector2<int>(100, 200));
+  ASSERT_TRUE (*this->vector2 == sameCoordinates) << "Should ignore the type and return true to `==` because all coordinate values are the same";
+  ASSERT_TRUE (*this->vector2 != diffCoordinates) << "Should ignore the type and return true to `!=` because one of the coordinate values are different";
 
-
-
-  // Should be true because they both have the same values and type
-  ASSERT_TRUE (Vector2<unsigned int>(100, 200) == Vector2<unsigned int>(100, 200));
-
-  // Should be false because they both have the same values
-  ASSERT_FALSE (Vector2<unsigned int>(100, 200) != Vector2<unsigned int>(100, 200));
-
-
-
-  // Should be true because they both have the same values and type
-  ASSERT_TRUE (Vector2<float>(100.5f, 200.5f) == Vector2<float>(100.5f, 200.5f));
-
-  // Should be false because they both have the same values
-  ASSERT_FALSE (Vector2<float>(100.5f, 200.5f) != Vector2<float>(100.5f, 200.5f));
-
-
-  // Variable of type Vector2 can be compared as well
-  Vector2<int> left {100, 200};
-  Vector2<int> right {100, 200};
-
-  ASSERT_TRUE(left == right);
-  ASSERT_FALSE(left != right);
-
+  ASSERT_FALSE (*this->vector2 == diffCoordinates) << "Should ignore the type and return false to `==` because one of the coordinate values are different";
+  ASSERT_FALSE (*this->vector2 != sameCoordinates) << "Should ignore the type and return false to `!=` because all coordinate value are the same";
 }
 
-TEST(Vector2, should_vector2_has_the_most_common_types_defined_as_shortcut) {
-  // For the sake of readability
-  using namespace Capstone;
+TYPED_TEST(Vector2Test, Vector2_Coordinates_ShouldAcceptAssignmentAsWellAsTheirTypes)
+{
+  this->Assign (new ::Capstone::Vector2<TypeParam> {100});
 
-  // Should be true because they both have the same values and type
-  // iVector == Vector<int>
-  ASSERT_TRUE (iVector2(100, 200) == Vector2<int>(100, 200));
+  ASSERT_EQ(this->vector2->x, 100) << "The Vector2's x coordinate should be 100 because single parameter constructor is called";
+  ASSERT_EQ(this->vector2->y, 100) << "The Vector2's y coordinate should be 100 because single parameter constructor is called";
 
-  // Should be true because they both have the same values and type
-  // uVector == Vector<unsigned int>
-  ASSERT_TRUE (uVector2(100, 200) == Vector2<unsigned int>(100, 200));
+  this->vector2->x += 1;
+  this->vector2->y -= 1;
 
-  // should be true because they both have the same values and type
-  // fVector == Vector<float>
-  ASSERT_TRUE (fVector2(100.5f, 200.5f) == Vector2<float>(100.5f, 200.5f));
+  ASSERT_EQ(this->vector2->x, 101) << "The Vector2's x coordinate should be 101 because `+=` operator was used";
+  ASSERT_EQ(this->vector2->y, 99) << "The Vector2's y coordinate should be 99 because `-=` operator was used";
+
+  ++this->vector2->x;
+  --this->vector2->y;
+
+  ASSERT_EQ(this->vector2->x, 102) << "The Vector2's x coordinate should be 102 because `++` operator was used";
+  ASSERT_EQ(this->vector2->y, 98) << "The Vector2's y coordinate should be 99 because `--` operator was used";
+
+  this->vector2->x *= 2;
+  this->vector2->y /= 2;
+
+  ASSERT_EQ(this->vector2->x, 204) << "The Vector2's x coordinate should be 204 because `*=` operator was used";
+  ASSERT_EQ(this->vector2->y, 49) << "The Vector2's y coordinate should be 48 because `/=` operator was used";
+
+  this->vector2->x = 0;
+  this->vector2->y = 0;
+
+  ASSERT_EQ(this->vector2->x, 0) << "The Vector2's x coordinate should be 0 because `=` operator was used";
+  ASSERT_EQ(this->vector2->y, 0) << "The Vector2's y coordinate should be 0 because `=` operator was used";
 }
 
-TEST(Vector2, should_vector2_coordinates_to_accept_assignment_as_well_as_their_types) {
-  // For the sake of readability
-  using namespace Capstone;
-
-  iVector2 vector2;
-
-  // Should accept additional assignment
-  vector2.x += 10;
-  ASSERT_EQ(10, vector2.x);
-
-  // Should accept subtraction assignment
-  vector2.x -= 5;
-  ASSERT_EQ(5, vector2.x);
-
-  // Should accept multiplication assignment
-  vector2.x *= 2;
-  ASSERT_EQ(10, vector2.x);
-
-  // Should accept division assignment
-  vector2.x /= 2;
-  ASSERT_EQ(5, vector2.x);
-
-  // Should accept module assignment
-  vector2.x %= 2;
-  ASSERT_EQ(1, vector2.x);
-}
-
-}
+} // namespace
