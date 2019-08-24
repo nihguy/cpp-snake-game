f18ef389272717a9d2b45a3454105b5ce81ad856
Victor Borges <victor.w.developer@gmail.com>
2019-08-24 18:20:34 -0300

    Improve the test description

--- tests/core/allocation_test.cc
+++ tests/core/allocation_test.cc
@@ -1,219 +1,168 @@
 #include "../../src/core/allocation.hpp"
+
 #include "gtest/gtest.h"
 
 namespace
 {
 
-TEST(Allocation, should_define_size_and_offset_as_zero_by_default) {
-  // For the sake of readability
-  using namespace Capstone;
+/**
+ * This allows the Allocation to avoid repeat each test for each template type.
+ *
+ * @tparam T  The type that will be assigned automatically by the GTest
+ */
+template <typename T>
+class AllocationTest: public ::testing::Test
+{
+ protected:
 
-  // The int allocation should defines zero to size and offset by default
-  Allocation<int> iallocation;
-  ASSERT_EQ (iVector2(0, 0), iallocation.size); // size is a Vector2
-  ASSERT_EQ (iVector2(0, 0), iallocation.offset); // offset is a Vector2
+  /**
+   * It allows this class to assign the Allocation's pointer to the variable `allocation` in other to automatic
+   * convert the template class using GTest template features.
+   *
+   * @param vector2  A pointer to the Dynamic Allocation type
+   */
+  void Assign (::Capstone::Allocation<T> *vector2) {
+    this->allocation = ::std::shared_ptr<::Capstone::Allocation<T>> (vector2);
+  }
+
+ protected:
+  /**
+   * This holds the Allocation instance used by the test properly
+   */
+  std::shared_ptr<Capstone::Allocation<T>> allocation;
+};
+
+/**
+ * This defines the most common types used in Allocation. It allows the GTest
+ * to convert the class automatically, so we can define the test just once
+ * and check the results for each Allocation's type.
+ */
+using AllocationTypes = ::testing::Types<int, unsigned int, float>;
+TYPED_TEST_SUITE (AllocationTest, AllocationTypes);
 
 
-  // The unsigned int allocation should defines zero to size and offset by default
-  Allocation<unsigned int> uallocation;
-  ASSERT_EQ (uVector2(0, 0), uallocation.size); // size is a Vector2
-  ASSERT_EQ (uVector2(0, 0), uallocation.offset); // offset is a Vector2
+TYPED_TEST(AllocationTest, DefaultContructor_ShouldDefineTheMeasuresAsZero)
+{
+  this->Assign (new ::Capstone::Allocation<TypeParam>{});
 
-
-  // The float allocation should defines zero to size and offset by default
-  Allocation<float> fallocation;
-  ASSERT_EQ (fVector2(0, 0), fallocation.size); // size is a Vector2
-  ASSERT_EQ (fVector2(0, 0), fallocation.offset); // offset is a Vector2
+  ASSERT_EQ(this->allocation->size, ::Capstone::Vector2<TypeParam>{}) << "Allocation's size coordinates should be zero when the default constructor is called";
+  ASSERT_EQ(this->allocation->offset, ::Capstone::Vector2<TypeParam>{}) << "Allocation's offset coordinates should be zero when the default constructor is called";
 }
 
-TEST(Allocation, should_allocation_constructor_to_define_the_offset_and_size_based_on_type_deduction) {
-  // For the sake of readability
-  using namespace Capstone;
 
-  // Should defines size and offset of the same type deduction
-  // (they both as int). The first argument should represents
-  // the size and the second one the offset.
-  Allocation<int> iallocation{{100, 200}, {300, 400}};
-  ASSERT_EQ (iVector2(100, 200), iallocation.size);
-  ASSERT_EQ (iVector2(300, 400), iallocation.offset);
+TYPED_TEST(AllocationTest, ParameterContructor_ShouldAssignImplicitVector2WithTheSameTypeAsArgument)
+{
+  this->Assign (new ::Capstone::Allocation<TypeParam>{{ 1, 2 }, { 3, 4 }}); // < Implicit Vector2<TypeParam> arguments
 
-
-  // Should defines size and offset of the same type deduction
-  // (they both as unsigned int). The first argument should represents
-  // the size and the second one the offset.
-  Allocation<unsigned int> uallocation{{100, 200}, {300, 400}};
-  ASSERT_EQ (uVector2(100, 200), uallocation.size);
-  ASSERT_EQ (uVector2(300, 400), uallocation.offset);
-
-  // Should defines size and offset of the same type deduction
-  // (they both as float). The first argument should represents
-  // the size and the second one the offset.
-  Allocation<float> fallocation{{100.5f, 200.5f}, {300.5f, 400.5f}};
-  ASSERT_EQ (fVector2(100.5f, 200.5f), fallocation.size); // X coordinate
-  ASSERT_EQ (fVector2(300.5f, 400.5f), fallocation.offset); // Y coordinate
+  ASSERT_EQ (this->allocation->size, ::Capstone::Vector2<TypeParam> (1, 2)) << "Allocation should accept implicit Vector2 with the same type as `size` arguments";
+  ASSERT_EQ (this->allocation->offset, ::Capstone::Vector2<TypeParam> (3, 4)) << "Allocation should accept implicit Vector2 with the same type as `offset` arguments";
 }
 
-TEST(Allocation, should_convert_a_allocation_type_to_another_using_constructor) {
-  // For the sake of readability
-  using namespace Capstone;
+TYPED_TEST(AllocationTest, ParameterContructor_ShouldAssignExplicitVector2WithTheSameTypeAsArgument)
+{
+  this->Assign (new ::Capstone::Allocation<TypeParam>{
+      ::Capstone::Vector2<TypeParam> { 1, 2 },
+      ::Capstone::Vector2<TypeParam> { 3, 4 }});
 
-  Allocation<int> iallocation{Allocation<float>{{ 100.5f, 200.5f }, { 300.5f, 400.5f }}};
-
-  // Should converts float to int
-  ASSERT_EQ (iVector2(100, 200), iallocation.size);
-  ASSERT_EQ (iVector2(300, 400), iallocation.offset);
+  ASSERT_EQ (this->allocation->size, ::Capstone::Vector2<TypeParam> (1, 2)) << "Allocation should accept explicit Vector2 with the same type as `size` arguments";
+  ASSERT_EQ (this->allocation->offset, ::Capstone::Vector2<TypeParam> (3, 4)) << "Allocation should accept explicit Vector2 with the same type as `offset` arguments";
 }
 
-TEST(Allocation, should_reassign_the_size_and_offset_using_public_variables) {
-  // For the sake of readability
-  using namespace Capstone;
+TYPED_TEST(AllocationTest, CopyContructor_ShouldCastTheCopiedVector2ToTheCurrentType)
+{
+  auto original = ::Capstone::Allocation<double>{{ 100.5f, 200.5f }, { 300.5f, 400.5f }};
+  this->Assign (new ::Capstone::Allocation<TypeParam>{original});
 
-  // Should allows the int Allocation class to defines their variables
-  Allocation<int> iallocation;
-  iallocation.size = iVector2(100, 200); // Vector2 must have the same deduction type
-  iallocation.offset = iVector2(100, 200); // Vector2 must have the same deduction type
+  ASSERT_STRNE(typeid(*this->allocation).name(), typeid(original).name()) << "The Allocation type should not be the same because it was converted";
 
-  ASSERT_EQ (iVector2(100, 200), iallocation.size);
-  ASSERT_EQ (iVector2(100, 200), iallocation.offset);
+  auto converted = this->allocation->template get_allocation_as<double> ();
+  ASSERT_STREQ(typeid(*this->allocation).name(), typeid(converted).name()) << "The Allocation type should be the same because it was converted";
 
-
-  // Should allows the unsigned int Allocation class to defines their variables
-  Allocation<unsigned int> uallocation;
-  uallocation.size = uVector2(100, 200); // Vector2 must have the same deduction type
-  uallocation.offset = uVector2(100, 200); // Vector2 must have the same deduction type
-
-  ASSERT_EQ (uVector2(100, 200), uallocation.size);
-  ASSERT_EQ (uVector2(100, 200), uallocation.offset);
-
-
-  // Should allows the float Allocation class to defines their variables
-  Allocation<float> fallocation;
-  fallocation.size = fVector2(100.5f, 200.5f); // Vector2 must have the same deduction type
-  fallocation.offset = fVector2(100.5f, 200.5f); // Vector2 must have the same deduction type
-
-  ASSERT_EQ (fVector2(100.5f, 200.5f), fallocation.size);
-  ASSERT_EQ (fVector2(100.5f, 200.5f), fallocation.offset);
+  ASSERT_TRUE(*this->allocation == original) << "Should be true because they both have the same measures";
 }
 
-TEST(Vector2, should_allocation_be_compared_with_another_one_with_the_same_type) {
-  // For the sake of readability
-  using namespace Capstone;
+TYPED_TEST(AllocationTest, Measures_ShouldBeReassigned)
+{
+  this->Assign (new ::Capstone::Allocation<TypeParam>{});
 
-  // This defines two allocations with the same value
-  auto ileft = Allocation<int>(iVector2{ 100, 200 }, iVector2{ 300, 400 });
-  auto iright = Allocation<int>(iVector2{ 100, 200 }, iVector2{ 300, 400 });
+  this->allocation->size = ::Capstone::Vector2<TypeParam>(100, 200);
+  this->allocation->offset = ::Capstone::Vector2<TypeParam>(300, 400);
 
-  ASSERT_TRUE (ileft == iright); // Should be true because they both have the same values and type
-  ASSERT_FALSE (ileft != iright); // Should be false because they both have the same values
-
-
-
-  // This defines two allocations with the same value
-  auto uleft = Allocation<unsigned int>(uVector2{ 100, 200 }, uVector2{ 300, 400 });
-  auto uright = Allocation<unsigned int>(uVector2{ 100, 200 }, uVector2{ 300, 400 });
-
-  ASSERT_TRUE (uleft == uright); // Should be true because they both have the same values and type
-  ASSERT_FALSE (uleft != uright); // Should be false because they both have the same values
-
-
-
-  // This defines two allocations with the same value
-  auto fleft = Allocation<float>(fVector2{ 100.5f, 200.5f }, fVector2{ 300.5f, 400.5f });
-  auto fright = Allocation<float>(fVector2{ 100.5f, 200.5f }, fVector2{ 300.5f, 400.5f });
-
-  ASSERT_TRUE (fleft == fright); // Should be true because they both have the same values and type
-  ASSERT_FALSE (fleft != fright); // Should be false because they both have the same values
+  ASSERT_EQ (this->allocation->size, ::Capstone::Vector2<TypeParam>(100, 200)) << "Allocation's size should be reassigned because they both have the same type";
+  ASSERT_EQ (this->allocation->offset, ::Capstone::Vector2<TypeParam>(300, 400)) << "Allocation's offset should be reassigned because they both have the same type";
 }
 
-TEST(Allocation, should_allocation_has_the_most_common_types_defined_as_shortcut) {
-  // For the sake of readability
-  using namespace Capstone;
+TYPED_TEST(AllocationTest, IsEqualToAndIsNotEqualToOperators_ShouldAllocationBeComparedToEachOther)
+{
+  this->Assign (new ::Capstone::Allocation<TypeParam>{{100, 200}, {300, 400}});
 
+  ASSERT_TRUE (*this->allocation == ::Capstone::Allocation<TypeParam>({100, 200}, {300, 400})) << "Should be true to `==` because they both have the same measures";
+  ASSERT_FALSE (*this->allocation != ::Capstone::Allocation<TypeParam>({100, 200}, {300, 400})) << "Should be false to `!=` because they both have the same measures";
 
-  // This defines two allocations with the same value
-  auto ileft = Allocation<int>({ 100, 200 }, { 300, 400 });
-  auto iright = iAllocation({ 100, 200 }, { 300, 400 });
-
-  // Should be true because they both have the same values and type
-  // iAllocation == Allocation<int>
-  ASSERT_TRUE (ileft == iright);
-
-
-  // This defines two allocations with the same value
-  auto uleft = Allocation<unsigned int>({ 100, 200 }, { 300, 400 });
-  auto uright = uAllocation({ 100, 200 }, { 300, 400 });
-
-  // Should be true because they both have the same values and type
-  // iAllocation == Allocation<unsigned int>
-  ASSERT_TRUE (uleft == uright);
-
-
-  // This defines two allocations with the same value
-  auto fleft = Allocation<float>({ 100.5f, 200.5f }, { 300.5f, 400.5f });
-  auto fright = fAllocation({ 100.5f, 200.5f }, { 300.5f, 400.5f });
-
-  // Should be true because they both have the same values and type
-  // fAllocation == Allocation<float>
-  ASSERT_TRUE (fleft == fright);
-
+  ASSERT_TRUE (*this->allocation == ::Capstone::Allocation<double>({100.0, 200.0}, {300.0, 400.0})) << "Should be true to `==` because they both have the same measures";
+  ASSERT_FALSE (*this->allocation != ::Capstone::Allocation<double>({100.0, 200.0}, {300.0, 400.0})) << "Should be false to `!=` because they both have the same measures";
 }
 
-TEST(Allocation, should_size_and_offset_can_be_reassigned) {
-  // For the sake of readability
-  using namespace Capstone;
+TYPED_TEST(AllocationTest, ShouldOffersTheMostCommonAlocationTypeAsShortcut)
+{
+  this->Assign (new ::Capstone::Allocation<TypeParam>{{100, 200}, {300, 400}});
 
-  iAllocation allocation;
+  if (typeid (TypeParam) == typeid (int))
+  {
+    auto iShortcut = ::Capstone::iAllocation({100, 200}, {300, 400});
+    ASSERT_STREQ (typeid (*this->allocation).name(), typeid (iShortcut).name()) << "The iAllocation should be a shortcut to Allocation<int>";
+  }
 
-  // Should accept additional assignment
-  allocation.size = iVector2{100, 200};
-  allocation.offset = iVector2{300, 400};
+  if (typeid (TypeParam) == typeid (unsigned int))
+  {
+    auto uShortcut = ::Capstone::uAllocation({100, 200}, {300, 400});
+    ASSERT_STREQ (typeid (*this->allocation).name(), typeid (uShortcut).name()) << "The uAllocation should be a shortcut to Allocation<unsigned int>";
+  }
 
-  ASSERT_EQ (iVector2(100, 200), allocation.size);
-  ASSERT_EQ (iVector2(300, 400), allocation.offset);
+  if (typeid (TypeParam) == typeid (float))
+  {
+    auto fShortcut = ::Capstone::fAllocation({100.f, 200.f}, {300.f, 400.f});
+    ASSERT_STREQ (typeid (*this->allocation).name(), typeid (fShortcut).name()) << "The fAllocation should be a shortcut to Allocation<float>";
+  }
 }
 
-TEST(Allocation, should_get_allocation_as_method_to_convert_the_current_allocation_to_another_one) {
-  // For the sake of readabilitskypey
-  using namespace Capstone;
+TYPED_TEST(AllocationTest, GetAllocationAs_ShouldMethodCastTheCurrentTypeUsingTemplateArgumentDeductionType)
+{
+  this->Assign (new ::Capstone::Allocation<TypeParam>{});
 
-  fAllocation fallocation({100.5f, 200.5f}, {300.5f, 400.5f});
-  iAllocation iallocation({100, 200}, {300, 400});
+  auto expected = ::Capstone::Allocation<double>{};
+  ASSERT_STRNE(typeid(*this->allocation).name(), typeid(expected).name()) << "The Allocations should not be the same type because it was converted";
 
-  ASSERT_EQ (iallocation, fallocation.get_allocation_as<int>());
+  auto converted = this->allocation->template get_allocation_as<double> ();
+  ASSERT_STREQ(typeid(converted).name(), typeid(expected).name()) << "The Allocations should be the same type because it was converted";
+
+  ASSERT_EQ(*this->allocation, converted) << "The allocation should be equal because they both have the same measures";
 }
 
-TEST(Allocation, should_get_bouding_box_return_the_object_edge_offset) {
-  // For the sake of readability
-  using namespace Capstone;
+TYPED_TEST(AllocationTest, GetBoundigBox_ShouldMethodReturnTheObjectEdgeOffset)
+{
+  this->Assign (new ::Capstone::Allocation<TypeParam>{{10, 10}, {10, 10}});
 
-  iAllocation iallocation({10, 10}, {10, 10});
+  ::Capstone::BoundingBox<TypeParam> boundingBox = this->allocation->get_bounding_box ();
 
-  auto bounding_box = iallocation.get_bounding_box ();
-
-  ASSERT_EQ (100, bounding_box.top);
-  ASSERT_EQ (110, bounding_box.bottom);
-  ASSERT_EQ (100, bounding_box.left);
-  ASSERT_EQ (110, bounding_box.right);
+  ASSERT_EQ (100, boundingBox.top) << "Should return true because top edge is the product of offset and size `y` coordinates";
+  ASSERT_EQ (110, boundingBox.bottom) << "Should return true because bottom edge is the product of offset and size `y` coordinates plus `y` size";
+  ASSERT_EQ (100, boundingBox.left) << "Should return true because left edge is the product of offset and size `x` coordinates";
+  ASSERT_EQ (110, boundingBox.right) << "Should return true because right edge is the product of offset and size `x` coordinates plus `x` size";
 }
 
-TEST(Allocation, should_check_collision_with_another_allocation) {
-  // For the sake of readability
-  using namespace Capstone;
+TYPED_TEST(AllocationTest, CheckCollision_ShouldMethodCheckAllocationCollisionBetweenEachOther)
+{
+  this->Assign (new ::Capstone::Allocation<TypeParam>{{10, 10}, {30, 40}});
 
-  fAllocation fallocation_over({10.f, 10.f}, {30.9f, 40.9f});
-  iAllocation iallocation_over({10, 10}, {30, 40});
+  ::Capstone::fAllocation over({10.f, 10.f}, {30.9f, 40.9f});
 
-  // Should return true because the allocation is over another one
-  ASSERT_TRUE (fallocation_over.check_collision(iallocation_over));
-  ASSERT_TRUE (iallocation_over.check_collision(fallocation_over));
+  ASSERT_TRUE (over.check_collision(*this->allocation)) << "Should return true because the allocation is colliding each other";
+  ASSERT_TRUE (this->allocation->check_collision(over)) << "Should return true because the allocation is colliding each other";
 
+  ::Capstone::fAllocation out({10.f, 10.f}, {20.f, 20.f});
 
-
-  fAllocation fallocation_out({10.f, 10.f}, {20.f, 20.f});
-  iAllocation iallocation_out({10, 10}, {30, 30});
-
-  // Should return false because the allocation is not touching another one
-  ASSERT_FALSE (fallocation_out.check_collision(iallocation_out));
-  ASSERT_FALSE (iallocation_out.check_collision(fallocation_out));
+  ASSERT_FALSE (out.check_collision(*this->allocation)) << "Should return false because the allocation is not colliding each other";
+  ASSERT_FALSE (this->allocation->check_collision(out)) << "Should return false because the allocation is not colliding each other";
 }
 }
