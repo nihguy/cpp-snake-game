34b98de5874e0532974e3963b9512a982ce07795
Victor Borges <victor.w.developer@gmail.com>
2019-09-02 12:51:26 -0300

    fix: Improve the documentation and the algorithm

--- src/states/splash_screen_state.cc
+++ src/states/splash_screen_state.cc
@@ -22,6 +22,7 @@ void SplashScreenState::update (std::size_t delta_time)
     m_game->push_state (std::make_unique<MainState> ());
   }
 
+  // It creates the fade out effect.
   if (m_elapsed_time >= (m_timeout - 600))
   {
     Color color = Color(10, 10, 10, 0);
@@ -31,6 +32,7 @@ void SplashScreenState::update (std::size_t delta_time)
     m_course_name_font->color += color;
   }
 
+  // It's important to change the font color before its update
   m_udacity_font->update (delta_time);
   m_capstone_font->update (delta_time);
   m_course_name_font->update (delta_time);
@@ -49,31 +51,41 @@ void SplashScreenState::render (Renderer& renderer)
 
 void SplashScreenState::prepare (Renderer& renderer)
 {
-  m_udacity_font = std::move(m_game->get_font ()->create (FontTheme::kLight, FontSize::kSmall));
-  m_capstone_font = std::move(m_game->get_font ()->create (FontTheme::kLight, FontSize::kMedium));
-  m_course_name_font = std::move(m_game->get_font ()->create (FontTheme::kLight, FontSize::kExtraSmall));
+  // This defines the screen center
+  iVector2 centered = {
+      renderer.get_screen ().x / 2,
+      renderer.get_screen ().y / 2,
+  };
 
+  // It get the smart pointer of each font using the third party library.
+  m_udacity_font = std::move(m_game->get_font ()->create (FontTheme::kLight, FontSize::kSmall));
+  m_capstone_font = std::move(m_game->get_font ()->create (FontTheme::kLight, FontSize::kLarge));
+  m_course_name_font = std::move(m_game->get_font ()->create (FontTheme::kLight, FontSize::kSmall));
+
+  // Assigning the text before prepare the font to get the correct Allocation
+  // of the font's texture. IT'S NECESSARY TO GET THE MEASURES
   m_udacity_font->set_text ("Udacity's");
   m_capstone_font->set_text ("Capstone");
-  m_course_name_font->set_text ("C++ NANODEGREE");
+  m_course_name_font->set_text ("C++ Nanodegree");
 
   m_udacity_font->prepare (renderer);
   m_capstone_font->prepare (renderer);
   m_course_name_font->prepare (renderer);
 
-  iVector2 screen_center = {
-    renderer.get_screen ().x / 2,
-    renderer.get_screen ().y / 2,
-  };
+  // This defines the "capstone"'s font text alignment
+  m_capstone_font->offset = centered;
+  m_capstone_font->offset.x -= m_capstone_font->size.x / 2;
+  m_capstone_font->offset.y -= m_capstone_font->size.y / 2 + 10;
 
-  m_capstone_font->offset.x = screen_center.x - m_capstone_font->size.x / 2;
-  m_capstone_font->offset.y = screen_center.y - m_capstone_font->size.y / 2 - 50;
+  // This defines "udacity"'s font text alignment
+  m_udacity_font->offset = centered;
+  m_udacity_font->offset.x -= m_udacity_font->size.x / 2;
+  m_udacity_font->offset.y = m_capstone_font->offset.y - 45;
 
-  m_udacity_font->offset.x = screen_center.x - m_udacity_font->size.x / 2;
-  m_udacity_font->offset.y = screen_center.y - m_capstone_font->size.y - 60;
-
-  m_course_name_font->offset.x = screen_center.x - m_course_name_font->size.x / 2;
-  m_course_name_font->offset.y = screen_center.y + m_capstone_font->size.y - 40;
+  // This defines the "course name"'s font text alignment
+  m_course_name_font->offset = centered;
+  m_course_name_font->offset.x -= m_course_name_font->size.x / 2;
+  m_course_name_font->offset.y = m_capstone_font->offset.y + m_capstone_font->size.y + 15;
 }
 
 void SplashScreenState::handle_input (const KeyPressed& key)
--- src/states/splash_screen_state.hpp
+++ src/states/splash_screen_state.hpp
@@ -7,22 +7,80 @@
 namespace Capstone
 {
 
+/**
+ * This is a simple GameState to display some information about the aim
+ * of this project.
+ */
 class SplashScreenState: public GameState
 {
  public:
-
-  SplashScreenState(std::size_t timeout = 4000);
+  /**
+   * This displays the Capstone information along the milliseconds defined
+   * as argument and jump to the next GameState when the this time is over.   *
+   *
+   * @param timeout  This define the millisecond used until change the focus
+   *                 to the next GameState
+   */
+  explicit SplashScreenState(std::size_t timeout = 4000);
   /**
    * Destructor
    */
   virtual ~SplashScreenState () = default;
 
+  /**
+   * This updates the GameObject's logic.
+   *
+   * It's invoked at every game loop cycle, respecting the interval time
+   * assigned as arguments by the Game class.
+   *
+   * It checks if the timeout is over and change the font's color, creating
+   * a simple fade out effect to them.
+   *
+   * @param delta_time  The elapsed time since the last game loop.
+   */
   void update (std::size_t delta_time) override;
+
+  /**
+   * This Renders the GameObjects
+   *
+   * It's invoked at every game loop cycle to show the game object on
+   * the screen. The fonts are rendered properly, on this method.
+   *
+   * @param renderer  The Renderer instance used to clear the last
+   *                  frame and show the object on the screen.
+   */
   void render (Renderer& renderer) override;
+
+
+  /**
+   * This prepares the GameObject to be rendered.
+   *
+   * As the GameState shouldn't hold the Renderer's reference, the following
+   * method helps the current class to get access to that. The fonts are
+   * aligned on the screen using this method.
+   *
+   * @param renderer  The Renderer instance to configure the GameObject.
+   */
   void prepare (Renderer& renderer) override;
+
+
+  /**
+   * It allows the current class to define its own input handler
+   *
+   * Basically, if the user press any allowed key the current class will
+   * skip the timeout defined and change the focus to the next GameState,
+   * immediately.
+   *
+   * @param key  The command dispatched that can be used to handle the GameObjects
+   */
   void handle_input (const KeyPressed& key) override;
 
  protected:
+  /**
+   * This variables defines the current GameState's lifetime.
+   * When the m_elapsed_time is greater than m_timeout, the current GameState
+   * stop to show the font's text and change the focus to the next GameState.
+   */
   std::size_t m_timeout;
   std::size_t m_elapsed_time;
 
